98d63b0bfb97f1d316bad993b3e3d599144a8392
Thorben Stangenberg <thorben.stangenberg@inoft.com>
2013-10-29 15:36:10 +0100

    added collectd and changed hostname to jira

--- manifests/default.pp
+++ manifests/default.pp
@@ -55,5 +55,27 @@ class jira {
   }
 
 }
-
 include jira
+
+
+include apt
+
+# this repo is needed vor collectd version 5.3
+apt::ppa { "ppa:vbulax/collectd5": }
+
+exec { "apt-update":
+  command => '/usr/bin/apt-get update',
+  user => root,
+  require => Apt::Ppa["ppa:vbulax/collectd5"],
+}
+
+
+class { '::collectd':
+  require => Exec["apt-update"]
+}
+
+include collectd
+
+class { 'collectd::plugin::write_graphite':
+  graphitehost => '10.0.2.2',
+}
